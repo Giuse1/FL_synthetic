8628220225be0723517041b7367d3f0d888f605a
Giuse1 <giuseppe.digiacomo@live.it>
2023-07-18 15:29:22 +0200

    modified config folder structure

---
+++ configs/cifar10_noniid2_vgg_val.py
@@ -0,0 +1,23 @@
+import ml_collections
+import torch
+import os
+
+from configs.cifar10_default import get_default_config, plot_config
+
+
+def get_config():
+    config = get_default_config()
+
+    config.data_distribution = "non_iid_2"
+    config.model = "vgg11"
+
+    config.validation_node = True
+
+
+    config.folder_logger = f"{config.root}/reports/{config.dataset}/val_node_{config.validation_node}/" \
+                           f"{config.data_distribution}_{config.model}_{config.optimizer}_{config.learning_rate}/" \
+                           f"cfg{config.cfg_scale}"
+
+    plot_config(config)
+
+    return config
